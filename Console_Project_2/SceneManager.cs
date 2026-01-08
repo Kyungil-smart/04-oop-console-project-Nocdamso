2bb93f571970ffc52aa6fc91ba52c7892834284a
Nocdamso <adsnmf@gmail.com>
2026-01-08 18:05:37 +0900

    commit2

--- Console_Project_2/SceneManager.cs
+++ Console_Project_2/SceneManager.cs
@@ -6,16 +6,25 @@ using System.Configuration;
 
 public class SceneManager
 {
+    public Action OnChangeScene;
     private Scene _current;
     public Scene Current
     {
         get => _current;
-        set
+        private set
         {
+            if (_current == value) return;
             _current = value;
             OnChangeScene?.Invoke();
         }
     }
 
-    public Action OnChangeScene;
+    private Dictionary<string, Scene> _scenes = new Dictionary<string, Scene>();
+    
+    public void AddScene(string key, Scene scene)
+    {
+        if (_scenes.ContainsKey(key)) return;
+
+        _scenes.Add(key, scene);
+    }
 }
